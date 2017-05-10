29c479acca15760bfc6e8071ed4c85fa8b1202c4
SC-201611152151\Administrator <friend3m@naver.com>
2017-05-10 11:47:13 +0900

    second 연습

--- Test5/src/test22.java
+++ Test5/src/test22.java
@@ -1,7 +1,9 @@
 
-public class test11 {
+public class test22 {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dfdfdf");
 		System.out.println("dfdfdf");
 		System.out.println("dfdfdf");
 	}
